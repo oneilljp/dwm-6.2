4e5b439a1c9040569fc8aa8b4a58a3a9a105edfc
oneilljp <oneilljp@umich.edu>
2021-05-25 01:40:03 -0400

    Cleaner dwm with more patches

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -7,7 +8,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0; 		/* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[] 			= { "Hack:size=10", "JetBrainsMonoNerdFont:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -23,25 +24,33 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "ﭮ", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     		    instance  title                tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    		    NULL,     NULL,                0,         1,          0,           0,        -1 },
+	{ "Wine",   			NULL,     NULL,       	   	   0,         1,          0, 		  -1, 		  0 },
+	{ NULL,					NULL,     "League of Legends", 1 << 0,    1,          0, 		  -1, 		 -1 },
+	{ "firefox",  			NULL,     NULL,       	       1 << 1,    0,          0, 		  -1, 		 -1 },
+	{ "Brave-browser",		NULL,     NULL,       	       1 << 1,    0,          0, 		  -1, 		 -1 },
+	{ "discord",  			NULL,     NULL,       	       1 << 2,    0,          0, 		  -1, 		  1 },
+	{ "Slack",  			NULL,     NULL,       	       1 << 8,    0,          0, 		  -1, 		  1 },
+	{ "Lutris",  			NULL,     NULL,       	       1 << 3,    0,          0, 		  -1, 		  1 },
+	{ "St",      		    NULL,     NULL,                0,         0,          1,           0,        -1 },
+	{ "kitty",     		    NULL,     NULL,                0,         0,          1,           0,        -1 },
+	{ "Alacritty", 		    NULL,     NULL,                0,         0,          1,           0,        -1 },
+	{ NULL,      		    NULL,     "Event Tester",      0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int attachdirection = 0; /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+static const int attachdirection = 2; /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 #include "fibonacci.c"
 static const Layout layouts[] = {
@@ -56,7 +65,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -69,15 +78,27 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "kitty", "--title", scratchpadname, NULL };
+
+//static const char *browcmd[] = { "firefox-developer-edition", NULL };
+//static const char *pbrowcomd[] = { "firefox-developer-edition","--private-window", NULL };
+static const char *browcmd[] = { "brave", NULL };
+static const char *pbrowcomd[] = { "brave","--incognito", NULL };
+static const char *upvol[] = { "pamixer", "-i", "5", NULL };
+static const char *downvol[] = { "pamixer", "-d", "5", NULL };
+static const char *mutevol[] = { "pamixer", "-t", NULL };
+static const char *bitwarden[] = { "rofi-rbw", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = bitwarden } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY, 						XK_w,	   spawn, 		   {.v = browcmd } },
+	{ MODKEY|ShiftMask, 			XK_w, 	   spawn, 		   {.v = pbrowcomd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -87,7 +108,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -113,7 +134,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+	{ 0,                       		XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                       		XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                       		XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
 };
 
 /* button definitions */
--- dwm.c
+++ dwm.c
@@ -260,7 +260,7 @@ static void zoom(const Arg *arg);
 
 static pid_t getparentprocess(pid_t p);
 static int isdescprocess(pid_t p, pid_t c);
-static int *Swallowingclient(Window w);
+static Client *swallowingclient(Window w);
 static Client *termforwin(const Client *c);
 static pid_t winpid(Window w);
 
